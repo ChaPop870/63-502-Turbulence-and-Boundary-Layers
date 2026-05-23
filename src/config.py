7ade1236a5368dd353db536babcc1a5d528e943d
drowe <daniel.rowe9193@gmail.com>
2026-05-23 14:25:15 +0200

    Added specific paths for testing in main branch.

--- src/config.py
+++ src/config.py
@@ -6,3 +6,10 @@ class Constants:
     bins = 100
 
     plot_storage_directory = Path("../plots")
+
+    r_temp_path = Path("../data/KPlane0500Temperature160000.nc")
+    r_w_path = Path("../data/KPlane0500VerticalVelocity.nc")
+
+    p_temp_path = Path("KPlane0500Temperature160000.nc")
+    p_w_path = Path("KPlane0500VerticalVelocity.nc")
+
